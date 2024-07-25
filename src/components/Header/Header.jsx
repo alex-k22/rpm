80fc93670f4df31bc907fc3a5d6b20d0cfa10cce
Alexey <alexey.kovalov@gmail.com>
2024-07-25 17:10:10 +0300

    header and nav changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,56 @@
 import Container from "../Shared/Container";
-import navLinks from "../../data/navMenu.json";
-import { NavHashLink } from "react-router-hash-link";
+import { useState, useEffect } from "react";
+
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
 import Logo from "./Logo";
 import css from "./Header.module.scss";
+import { RxHamburgerMenu, RxCross2  } from "react-icons/rx";
+import Navigation from "./Navigation";
 
 const Header = () => {
+  const [mobileMenuVisible, setMobilMenuVisible] = useState(false);
+  const { width } = useWindowDimensions();
+  const { visible } = css;
+
+  const handleMobileToggle = () => {
+    setMobilMenuVisible(!mobileMenuVisible);
+  };
+
+  const handleNavClick = (event) => {
+    if (event.target.tagName === "A") {
+      handleMobileToggle();
+    }
+  };
+
+  useEffect(() => {
+    if (mobileMenuVisible) {
+      document.body.classList.add("is-open");
+    }
+    if (!mobileMenuVisible) {
+      document.body.classList.remove("is-open");
+    }
+  }, [mobileMenuVisible]);
+
   return (
     <header className={css.header}>
       <Container>
         <div className={css.wrapper}>
           <Logo />
-          <nav>
-            <ul className={css.menu}>
-              {navLinks.map(({ title, href }) => {
-                return (
-                  <li key={href} className={css.li}>
-                    <NavHashLink
-                      to={`${href}`}
-                      activeClassName="selected"
-                      activeStyle={{ color: "red" }}
-                      className={css.link}
-                    >
-                      {title}
-                    </NavHashLink>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
+          {width > 1440 && <Navigation />}
+          {width < 1440 && (
+              <button
+                className={css.btn}
+                type="button"
+                onClick={handleMobileToggle}
+              >
+                {mobileMenuVisible ? (
+                  <RxCross2 className={css.burgerIcon} />
+                ) : (
+                  <RxHamburgerMenu className={css.burgerIcon} />
+                )}
+              </button>
+            )}
         </div>
       </Container>
     </header>
